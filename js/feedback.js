7b087946636c21916e8929a3825fac4c5b5abb8d
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2025-02-12 00:10:56 +0300

    slider animation; feedback form added

---
+++ js/feedback.js
@@ -0,0 +1,59 @@
+const popup = document.createElement('div');
+popup.className = 'feedback';
+popup.innerHTML = `
+  <form class="feedback__form">
+    <div class="feedback__close" onclick="closePopup()"></div>
+    <p class="feedback__title">Обратная связь</p>
+    <label for="feedback__name" class="feedback__label">Ваше имя *
+      <input class="feedback__input" type="input" name="feedback__name" id="feedback__name" required>
+    </label>
+    <label for="feedback__phone" class="feedback__label">Телефон *
+      <input class="feedback__input" type="tel" name="feedback__phone" id="feedback__phone" required>
+    </label>
+    <label for="feedback__email" class="feedback__label">Email *
+      <input class="feedback__input" type="email" name="feedback__email" id="feedback__email" required>
+    </label>
+    <label for="feedback__message" class="feedback__label">Сообщение
+      <textarea class="feedback__textarea" type="tel" name="feedback__message" id="feedback__message"></textarea>
+    </label>
+    <button class="feedback__button" type="button" onclick="validate()">Отправить</button>
+  </form>
+`;
+document.body.appendChild(popup);
+document.querySelector('.header__contact').addEventListener('click', openPopup);
+const inputs = popup.querySelectorAll('input');
+
+inputs.forEach(input => input.addEventListener('click', () => {
+  if (input.classList.contains('feedback__input_error')) {
+    input.classList.remove('feedback__input_error')
+    input.value = '';
+  }
+}));
+
+function openPopup() {
+  popup.style.display = 'flex';
+}
+
+function closePopup() {
+  popup.classList.add('feedback_fade');
+  setTimeout(() => {
+    popup.style.display = 'none';
+    popup.classList.remove('feedback_fade');
+  }, 400);
+}
+
+function validate() {
+  let valid = true;
+  inputs.forEach(input => {
+    if (!input.reportValidity()) {
+      valid = false;
+      if (input.required && input.value === '') {
+        input.classList.add('feedback__input_error');
+        input.value = 'Поле обязательно для заполнения';
+      }
+    }
+  });
+  if (valid) {
+    popup.querySelector('form').submit();
+  }
+}
--- js/slider.js
+++ js/slider.js
@@ -3,23 +3,58 @@ const slides = [
   './img/slide2.png',
 ]
 
-const slide = document.querySelector('.slider');
+const sliderWidth = 1248;
+
+const slider = document.querySelector('.slider__slide');
 const prev = document.querySelector('.slider__nav_prev');
 const next = document.querySelector('.slider__nav_next');
 
 let current = 0;
-setSlide();
+let timeout = null;
+slider.style.backgroundImage = `url('${slides[0]}')`;
+checkSizes();
 
-prev.addEventListener('click', () => {
-  current = current === 0 ? slides.length - 1 : current - 1;
-  setSlide();
-});
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
+window.addEventListener('resize', checkSizes);
 
-next.addEventListener('click', () => {
-  current = current === slides.length - 1 ? 0 : current + 1;
-  setSlide();
-});
+function prevSlide() {
+  const newSlide = current === 0 ? slides.length - 1 : current - 1;
+  setSlide(newSlide);
+}
 
-function setSlide() {  
-  slide.style.backgroundImage = `url('${slides[current]}')`;
+function nextSlide() {
+  const newSlide = current === slides.length - 1 ? 0 : current + 1;
+  setSlide(newSlide);
+}
+
+function setSlide(newSlide) {
+  slider.classList.add('slider__slide_fade');
+  setTimeout(() => {
+    slider.classList.remove('slider__slide_fade');
+    current = newSlide;
+    slider.style.backgroundImage = `url('${slides[current]}')`;
+  }, 1000);
+  prepareNext();
+}
+
+function checkSizes() {
+  if (window.innerWidth < sliderWidth) {
+    slider.style.display = 'none';
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = null;
+    }
+  }
+  else {
+    slider.style.display = 'block';
+    prepareNext();
+  }
+}
+
+function prepareNext() {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(nextSlide, 5000);
 }
